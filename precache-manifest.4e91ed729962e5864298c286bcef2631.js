1f3d66339b76636267bf01322fcafe1b835e21d7
ComIT-Ozzie <oz.neher+comit@gmail.com>
2019-12-12 18:24:54 -0600

    Updates

---
+++ precache-manifest.4e91ed729962e5864298c286bcef2631.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e23071f468cbf32b075140a2f681ef36",
+    "url": "/ozzies-example-project/index.html"
+  },
+  {
+    "revision": "b8e837a5e1aeef0d6719",
+    "url": "/ozzies-example-project/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "24fd062ddf9098c8dac1",
+    "url": "/ozzies-example-project/static/js/2.8d7475e3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ozzies-example-project/static/js/2.8d7475e3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b8e837a5e1aeef0d6719",
+    "url": "/ozzies-example-project/static/js/main.4a8395dc.chunk.js"
+  },
+  {
+    "revision": "adbe6c42cb4d896429d0",
+    "url": "/ozzies-example-project/static/js/runtime-main.1c7a9206.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ozzies-example-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
